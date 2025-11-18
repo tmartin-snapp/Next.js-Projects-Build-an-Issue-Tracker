27e241897b97451f2be5ef0152998bfe587f3dc7
tmartin-snapp <tmartin@snapp-solutions.com>
2025-11-18 16:55:10 +0100

    Adding Markdown Preview

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,13 +3,14 @@ import prisma from "@/prisma/client";
 import { Prisma } from "@prisma/client";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
+import ReactMarkdown from "react-markdown";
 
 interface Props {
   params: { id: string };
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
-  if (typeof parseInt(params.id) !== 'number') notFound();
+  if (typeof parseInt(params.id) !== "number") notFound();
 
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
@@ -21,10 +22,12 @@ const IssueDetailPage = async ({ params }: Props) => {
     <div>
       <Heading>{issue.title}</Heading>
       <Flex gap="5" my="2">
-        <IssueStatusBadge status={issue.status}/>
+        <IssueStatusBadge status={issue.status} />
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card>{issue.description}</Card>
+      <Card className="prose" mt="4">
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
     </div>
   );
 };
