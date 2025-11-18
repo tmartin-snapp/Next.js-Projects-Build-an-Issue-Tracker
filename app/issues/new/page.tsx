f7ad07e17db14e82d102a1f71b2816a4570e4327
tmartin-snapp <tmartin@snapp-solutions.com>
2025-11-18 13:52:05 +0100

    Adding a Spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,15 +2,17 @@
 
 import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
-import { useForm, Controller } from "react-hook-form";
+import { useForm, Controller, set } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/api/issues/validationSchemas";
 import { z } from "zod";
-import "easymde/dist/easymde.min.css";
 import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
+
+import "easymde/dist/easymde.min.css";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -25,6 +27,7 @@ const NewIssuePage = () => {
     resolver: zodResolver(createIssueSchema),
   });
   const [error, setError] = useState("");
+  const [isSubmitting, setSubmitting] = useState(false);
 
   return (
     <div className="max-w-xl">
@@ -37,9 +40,11 @@ const NewIssuePage = () => {
         className="space-y-3"
         onSubmit={handleSubmit(async (data) => {
           try {
+            setSubmitting(true);
             await axios.post("/api/issues", data);
             router.push("/issues");
           } catch (error) {
+            setSubmitting(false);
             setError("An unexplected error occurred. Please try again.");
           }
         })}
@@ -57,7 +62,7 @@ const NewIssuePage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
-        <Button>Submit New Issue</Button>
+        <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner />}</Button>
       </form>
     </div>
   );
