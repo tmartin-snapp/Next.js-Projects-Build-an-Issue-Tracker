a42616280f0a949c8e482fdac454bb08503b386b
tmartin-snapp <tmartin@snapp-solutions.com>
2025-11-18 16:36:12 +0100

    Showing Issue Details + Next v16 -> v13

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -30,10 +30,10 @@ const IssuesPage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className="block md:hidden">
                   <IssueStatusBadge status={issue.status} />
-                  </div>
+                </div>
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
                 <IssueStatusBadge status={issue.status} />
