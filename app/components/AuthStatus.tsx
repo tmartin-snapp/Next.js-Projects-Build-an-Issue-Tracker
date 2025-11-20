884853f4c8b61c71a71d436a9b00c3a262b0a11c
tmartin-snapp <tmartin@snapp-solutions.com>
2025-11-20 17:36:09 +0100

    Improving troubleshooting

---
+++ app/components/AuthStatus.tsx
@@ -0,0 +1,40 @@
+import { Box, DropdownMenu, Avatar, Text } from "@radix-ui/themes";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import React from "react";
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+
+  if (status === "loading") return null;
+
+  if (status === "unauthenticated")
+    return <Link href="/api/auth/signin">Log In</Link>;
+
+  return (
+    <Box>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user!.image!}
+            fallback="?"
+            size="2"
+            radius="full"
+            className="cursor-pointer"
+            referrerPolicy="no-referrer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <DropdownMenu.Label>
+            <Text size="2">{session!.user!.email}</Text>
+          </DropdownMenu.Label>
+          <DropdownMenu.Item>
+            <Link href="/api/auth/signout">Log Out</Link>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
+  );
+};
+
+export default AuthStatus;
