ac3b3692fd611b2bb5c3ce964cca0dd8d4cd5d8e
tmartin-snapp <tmartin@snapp-solutions.com>
2025-11-24 15:53:02 +0100

    Implementing Pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import Pagination from "./components/Pagination";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
-    <Pagination itemCount={200} pageSize={20} currentPage={1} />
-  )
+    <Pagination
+      itemCount={200}
+      pageSize={20}
+      currentPage={parseInt(searchParams.page)}
+    />
+  );
 }
