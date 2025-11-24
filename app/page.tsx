11905e1f21bd0c5af2069729c1e39634e0225cb3
tmartin-snapp <tmartin@snapp-solutions.com>
2025-11-24 15:41:27 +0100

    Building he Pagination Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import Pagination from "./components/Pagination";
+
 export default function Home() {
   return (
-    <div> hello world </div>
+    <Pagination itemCount={200} pageSize={20} currentPage={1} />
   )
 }
