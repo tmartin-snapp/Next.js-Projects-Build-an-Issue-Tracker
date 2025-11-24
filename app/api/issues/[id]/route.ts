091c56b22333fe262025b78832d930e0f77d39f0
tmartin-snapp <tmartin@snapp-solutions.com>
2025-11-24 13:34:34 +0100

    Implementing the API

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { IssueSchema } from "../validationSchemas";
+import { patchIssueSchema } from "../validationSchemas";
 import prisma from "@/prisma/client";
 import authOptions from "@/app/auth/authOptions";
 import { getServerSession } from "next-auth";
@@ -8,14 +8,28 @@ export async function PATCH(
   request: NextRequest, 
   { params }: {params: {id: string}}) 
   {
-    const session = await getServerSession(authOptions);
-    if (!session)
-      return NextResponse.json({}, {status: 401});
+    // const session = await getServerSession(authOptions);
+    // if (!session) return NextResponse.json({}, {status: 401});
 
     const body = await request.json();
-    const validation = IssueSchema.safeParse(body);
+    const validation = patchIssueSchema.safeParse(body);
     if (!validation.success)
-      return NextResponse.json(validation.error.errors, {status: 400})
+      return NextResponse.json(validation.error.errors, {
+        status: 400,
+      })
+
+    const { assignedToUserId, title, description } = body;
+    if (assignedToUserId) {
+      const user = await prisma.user.findUnique({
+        where: { id: assignedToUserId }
+      });
+      if (!user)
+        return NextResponse.json({ 
+          error: 'Invalid user.'
+        }, {
+          status: 400
+        })
+    }
 
     const issue = await prisma.issue.findUnique({
       where: { id: parseInt(params.id)}
@@ -25,10 +39,11 @@ export async function PATCH(
       return NextResponse.json({error: 'Invalid issue'}, {status: 400})
 
     const updatedIssue = await prisma.issue.update({
-      where: {id: issue.id},
+      where: { id: issue.id },
       data: {
-        title: body.title,
-        description: body.description
+        title,
+        description,
+        assignedToUserId
       }
     })
 
